79723e13f32c5f34c77fda27410f067d8df3ec2c
The Rissenator <mrissen@centriq.com>
2019-09-26 23:58:55 -0500

    Updated with latest version of library for Webinar 20190926

---
+++ AzureBlobStorageExample/AzureBlobStorageSample/AzureStorageAccount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorageSample
+{
+    public static class AzureStorageAccount
+    {
+        public static string GetConnectionString()
+        {
+            string ConfigFileContentString = File.ReadAllText("../../Configuration/config.json");
+            var Config = JsonConvert.DeserializeObject<Dictionary<string, string>>(ConfigFileContentString);
+            return Config["connectionString"];
+        }
+    }
+}
--- AzureBlobStorageExample/AzureBlobStorageSample/Program.cs
+++ AzureBlobStorageExample/AzureBlobStorageSample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -16,54 +15,50 @@ namespace AzureBlobStorageSample
         static void Main(string[] args)
         {
             //  Storage account information
+            //  Set yours in the app.config first by using
             //  information from azure management portal
-            string connectionInfo = AccountManager.GetAzureAccount();
+            string con = AzureStorageAccount.GetConnectionString();
 
             //  Create the storage client - this class has
             //  Methods related to managing the storage account
-            CloudStorageAccount storageClient = CloudStorageAccount.Parse(connectionInfo);
+            var storageClient = CloudStorageAccount.Parse(con);
 
             //  Create the blob client - this class has 
             //  Methods related to working with a specific blob
-            CloudBlobClient blobclient = storageClient.CreateCloudBlobClient();
+            var blobclient = storageClient.CreateCloudBlobClient();
 
             //  Get a reference to a specific container
-            CloudBlobContainer container = blobclient.GetContainerReference("20487-private-centriq");
+            var container = blobclient.GetContainerReference("bpics");
             container.CreateIfNotExists();
 
-            //  List out all of the items in this blob
-            IEnumerable<IListBlobItem> AllItems = container.ListBlobs();
-
             //  Get a reference to the blockblob called buymoria
-            CloudBlockBlob buymore = container.GetBlockBlobReference("buymoria");
-            buymore.DeleteIfExists();
+            var BeeContainer = container.GetBlockBlobReference("Bees");
 
             //  Upload the file by reading the entire thing into 
             //  an array of bytes (best if only used on small files)
-            byte[] bytes = File.ReadAllBytes(@"Content\buymore2.xml");
+            var bytes = File.ReadAllBytes(@"Content\queen-bee.jpg");
 
-            //  Upload the file to the blob
-            buymore.UploadFromByteArray(bytes, 0, bytes.Length);
+            ////  Upload the file to the blob
+            BeeContainer.UploadFromByteArray(bytes, 0, bytes.Length);
 
-            //  Upload the same file by using a filestream
+            //  You could also Upload the same file by using a filestream
             //  Best if the file is decently large (less memory)
-            var strm = new FileStream(@"Content\buymore2.xml", FileMode.Open);
-            buymore.UploadFromStream(strm);
-            strm.Close();
+            //var strm = new FileStream(@"Content\Queen-bee.jpg", FileMode.Open);
+            //BeeContainer.UploadFromStream(strm);
+            //strm.Close();
 
             //  Now we prepare to download it.
             //  create an array of bytes of the length needed
-            //  Use buymore.Properties.Length
-            //  Best if used only on small files.
-            byte[] dlbytes = new byte[buymore.Properties.Length];
-            int file = buymore.DownloadToByteArray(dlbytes, 0);
+            //  Use buymore.Property["length"]
+            byte[] dlbytes = new byte[BeeContainer.Properties.Length];
+            var file = BeeContainer.DownloadToByteArray(dlbytes, 0);
             File.WriteAllBytes(@"Content\DL-" + Guid.NewGuid().ToString() + ".xml", dlbytes);
 
-            //  Now do the same, but using a filestream 
-            //  best if used on large files (less memory)
-            FileStream wstrm = new FileStream(@"Content\DL-" + Guid.NewGuid().ToString() + ".xml", FileMode.Create);
-            buymore.DownloadToStream(wstrm);
-            wstrm.Close();
+            //  You could also Download the same file by using a filestream
+            //  Best if the file is decently large (less memory)
+            //FileStream wstrm = new FileStream(@"Content\DL-" + Guid.NewGuid().ToString() + ".jpg", FileMode.Create);
+            //BeeContainer.DownloadToStream(wstrm);
+            //wstrm.Close();
 
             //  Look in the Bin\Debug folder for these files.
             //  Both DL-* files should look exactly like the 
